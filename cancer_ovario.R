057531b99e279fae04eeae85454c3efb19713da3
YosefGuevara012 <yosef.guevara012@hotmail.com>
2021-03-08 17:28:19 -0500

    HARZARD FUNCTION ADDED

---
+++ cancer_ovario.R
@@ -0,0 +1,16 @@
+
+# Lectura de los datos de cancer de ovarios
+
+can_ovario <- read.csv("cancer_ovario.csv", header=T, sep=";")
+attach(can_ovario)
+
+# Cargar los paquetes survival y muhaz
+
+require(survival)
+require(muhaz)
+
+# Estimación de la funcion de riesgo
+
+
+riesgo.can <- muhaz(Tiempo, Censura)
+plot(riesgo.can)
